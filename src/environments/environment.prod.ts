7c3db19f73b1987e182904ffec40b82e13007420
Kamil <bil@int.pl>
2025-01-04 23:34:51 +0100

    Env variables.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'https://dog-breed-recognition-backend.kamilbil.pl',
+  clientId:
+    '661572792855-hb3p972otj75mqvsga1a3vqpaidiuvu0.apps.googleusercontent.com',
+  showDebugInformation: false,
 };
